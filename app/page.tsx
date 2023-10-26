07d7ef2a22745b40c709b3a92a3829a1c3bb74d7
Niroyan Kugaruban <48611787+yanniro2@users.noreply.github.com>
2023-10-26 16:11:15 +0530

    add keystatics

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,17 @@ import Card from "@/components/Card";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Used from "@/components/Used";
-import Image from "next/image";
-
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <>
+      <Header />
       <Hero />
       <Used />
       <Features />
       <Card />
+      <Footer />
     </>
   );
 }
