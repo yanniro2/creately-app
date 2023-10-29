1ba19df3e97a7ec2f0aae77af2e470dea2e4bae6
Niroyan Kugaruban <48611787+yanniro2@users.noreply.github.com>
2023-10-29 11:51:50 +0530

    fixed used mobile responsive

--- components/Used.tsx
+++ components/Used.tsx
@@ -3,17 +3,19 @@ import React from "react";
 
 const Used = () => {
   return (
-    <section className="w-screen py-[2rem]">
-      <div className="container mx-auto flex items-center justify-center flex-col gap-[2rem]">
-        <h1 className="h2">
+    <section className="w-screen py-8 md:py-16">
+      <div className="container mx-auto flex items-center justify-center flex-col gap-6 px-[1rem]">
+        <h1 className="text-lg md:text-2xl text-center">
           Over 5 Million people and 1000’s of teams already use Creately
         </h1>
-        <Image
-          src="/Assets/customer_logos.svg"
-          width={834}
-          height={65}
-          alt="company icons"
-        />
+        <div className="w-full md:w-auto">
+          <Image
+            src="/Assets/customer_logos.svg"
+            width={834}
+            height={65}
+            alt="company icons"
+          />
+        </div>
       </div>
     </section>
   );
