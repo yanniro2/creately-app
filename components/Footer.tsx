b8af1d73096d2bce72877ad5b6873a74f69eae9e
Niroyan Kugaruban <48611787+yanniro2@users.noreply.github.com>
2023-10-29 11:41:28 +0530

    footer responsive done

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,25 +1,29 @@
-import React from 'react'
-import Link from 'next/link';
-import Image from 'next/image';
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
 const Footer = () => {
   return (
-    <div className="custom-clip-path bg-blue-100 flex items-center justify-center ">
-      <footer className="container mx-auto flex items-center justify-between">
-        <div className="flex items-center text-light">
-          <span>Contact Support :</span> support@creately.com
-        </div>
-
-        <Link href={"/"}>
-          <Image
-            src="/Assets/creately_logo.svg"
-            alt="logo img"
-            width={166}
-            height={44}
-          />
-        </Link>
-      </footer>
+    <div className="w-full h-[50vh] flex items-end">
+      <div className="custom-clip-path bg-blue-100 flex items-end justify-center pb-[5%]">
+        <footer className="container mx-auto flex flex-col md:flex-row items-center justify-between p-4 md:p-0">
+          <div className="text-light text-center md:text-left mb-4 md:mb-0">
+            <span>Contact Support :</span> support@creately.com
+          </div>
+          <div className="mt-4 md:mt-0">
+            <Link href={"/"}>
+              <Image
+                src="/Assets/creately_logo.svg"
+                alt="logo img"
+                width={166}
+                height={44}
+              />
+            </Link>
+          </div>
+        </footer>
+      </div>
     </div>
   );
-}
+};
 
-export default Footer
+export default Footer;
